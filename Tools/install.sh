8620721dfba4abb5c0982e184553a3f9f621974c
Mikael Wågberg <mikael_wagberg@trimble.com>
2024-09-15 23:13:16 +0200

    Add installation, uninstallation, and update scripts for Askstatus System

---
+++ Tools/install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#Check if script is being run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [ ! $? = 0 ]; then
+   exit 1
+else
+
+   ./getlatest.sh
+
+   chmod +x /var/www/backend/Askstatus.Web.API
+
+   # create database folder
+   mkdir -p /usr/share/askstatus
+   sudo chmod 777 /usr/share/askstatus
+
+   # /var/www/backend/Askstatus.Web.API --seed
+
+   # cp askstatusbackend.service /etc/systemd/system
+   # if [ ! -f /etc/systemd/system/askstatusbackend.service ]; then
+   #   whiptail --title "Installation aborted" --msgbox "There was a problem writing the askstatusbackend.service file" 8 78
+   #  exit
+   # fi
+
+   # if [ ! -f /etc/nginx/sites-available/default.bak ]; then
+   #   cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+   # fi
+
+   # cp default /etc/nginx/sites-available/default
+   # if [ ! -f /etc/nginx/sites-available/default ]; then
+   #   whiptail --title "Installation aborted" --msgbox "There was a problem writing the default nginx config file" 8 78
+   #  exit
+   # fi
+   # nginx -s reload
+
+   # systemctl enable /etc/systemd/system/askstatusbackend.service
+   # systemctl start /etc/systemd/system/askstatusbackend.service
+   whiptail --title "Installation complete" --msgbox "Askstatus System installation complete." 8 78
+
+   #reboot
+   #poweroff
+fi
---
+++ Tools/uninstall.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#Check if script is being run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [ ! $? = 0 ]; then
+   exit 1
+else
+
+   # systemctl stop askstatusbackend.service
+   # systemctl disable /etc/systemd/askstatusbackend.service
+
+   # rm /etc/systemd/system/askstatusbackend.service
+
+   rm -f -r /var/www/backend
+   rm -f -r /var/www/frontend
+
+   whiptail --title "Uninstall complete" --msgbox "Askstatus System uninstall complete." 8 78
+fi
