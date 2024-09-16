b4cc312123b1b7a3b0ca7507485a9fded9b68686
Mikael Wågberg <mikael_wagberg@trimble.com>
2024-09-17 00:11:38 +0200

    Add new projects and configure solution structure
    Added new project files for `Askstatus.Application`, `Askstatus.Application.Tests`, `Askstatus.Architecture.Tests`, `Askstatus.Web.API`, and `Askstatus.Web.App` with .NET 8.0 configurations, nullable reference types, and necessary package references.
    
    Introduced `AssemblyReference` classes in `Askstatus.Application`, `Askstatus.Web.API`, and `Askstatus.Web.App` for static assembly references.
    
    Created `ArchitectureTests` class in `Askstatus.Architecture.Tests` to ensure no dependencies between `Askstatus.Application`, `Askstatus.Web.API`, `Askstatus.Web.App`, and test projects.
    
    Updated `askstatus.sln` to include new projects and organize them under `Core`, `Presentation`, and `Tests` solution folders.
    
    Configured build and debug settings for the new projects in the solution file.

---
+++ Askstatus.Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Askstatus.Architecture.Tests
+{
+    public class ArchitectureTests
+    {
+        private const string ApplicationNamespace = "Askstatus.Application";
+        private const string WebAPINamespace = "Askstatus.Web.API";
+        private const string WebUiNamespace = "Askstatus.Web.App";
+
+        private const string ApplicationTestsNamespace = "Askstatus.Application.Tests";
+        private const string ArchitectureTestsNamespace = "Askstatus.Architecture.Tests";
+        //private const string PersistenceTestsNamespace = "Askstatus.Persistence.Tests";
+        //private const string WebAPITestsNamespace = "Askstatus.Web.API.Tests";
+
+        [Fact]
+        public void Application_Shuld_Not_HaveDependencyOnOtherProjects()
+        {
+            var assembly = typeof(Askstatus.Application.AssemblyReference).Assembly;
+
+            var otherProjects = new[]
+            {
+                WebAPINamespace,
+                WebUiNamespace,
+                ArchitectureTestsNamespace,
+                ApplicationTestsNamespace,
+            };
+
+            var result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAny(otherProjects).GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+
+        }
+
+        [Fact]
+        public void WebAPI_Shuld_Not_HaveDependencyOnOtherProjects()
+        {
+            var assembly = typeof(Askstatus.Web.API.AssemblyReference).Assembly;
+
+            var otherProjects = new[]
+            {
+                WebUiNamespace,
+                ArchitectureTestsNamespace,
+                ApplicationTestsNamespace,
+            };
+
+            var result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAny(otherProjects).GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void WebUI_Shuld_Not_HaveDependencyOnOtherProjects()
+        {
+            var assembly = typeof(Askstatus.Web.App.AssemblyReference).Assembly;
+
+            var otherProjects = new[]
+            {
+                ArchitectureTestsNamespace,
+                ApplicationTestsNamespace,
+                WebAPINamespace,
+            };
+
+            var result = Types.InAssembly(assembly).ShouldNot().HaveDependencyOnAny(otherProjects).GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+    }
+}
