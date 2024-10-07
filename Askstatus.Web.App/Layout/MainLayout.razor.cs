869d014ed60a425eb2359cb3da4d5c2400f3cf73
Mikael Wågberg <mikael_wagberg@trimble.com>
2024-10-07 11:06:56 +0200

    Integrate MudBlazor and enhance testing setup
    Added `workflow_dispatch` trigger to GitHub Actions workflow in `build.yml` for manual triggering. Updated `dotnet test` command to include JUnit XML logger. Added `JunitXml.TestLogger` package to multiple test projects. Integrated `Microsoft.Extensions.Http` and `MudBlazor` packages in `Askstatus.Web.App.csproj`. Refactored `MainLayout.razor` and `NavMenu.razor` to use MudBlazor components. Added `MainLayout.razor.cs` for layout logic. Updated `Weather.razor` to use `IHttpClientFactory`. Configured MudBlazor services and `HttpClient` in `Program.cs`. Updated `_Imports.razor`, `app.css`, `index.html`, and `manifest.webmanifest` for MudBlazor integration. Added package versions in `Directory.Packages.props`. Corrected project type GUIDs in `askstatus.sln`. Updated version properties in `version.props`.

---
+++ Askstatus.Web.App/Layout/MainLayout.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Components;
+
+namespace Askstatus.Web.App.Layout;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    private bool _drawerOpen = false;
+    protected string Version { get; private set; } = string.Empty;
+
+    protected string AspDotnetVersion { get; private set; } = string.Empty;
+
+    private void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Assembly currentAssembly = typeof(MainLayout).Assembly;
+        if (currentAssembly == null)
+        {
+            currentAssembly = Assembly.GetCallingAssembly();
+        }
+
+        AspDotnetVersion = currentAssembly.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName!;
+        Version = $"{currentAssembly.GetName().Version!.Major}.{currentAssembly.GetName().Version!.Minor}.{currentAssembly.GetName().Version!.Build}";
+        await base.OnInitializedAsync();
+    }
+}
