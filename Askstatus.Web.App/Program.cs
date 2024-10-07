79e9088c1c1ca05b2707ac918a8d231268c7bb95
Mikael Wågberg <133635952+MWagber@users.noreply.github.com>
2024-10-07 12:56:53 +0200

    Add mudblazor (#16)
    * Integrate MudBlazor and enhance testing setup
    
    Added `workflow_dispatch` trigger to GitHub Actions workflow in `build.yml` for manual triggering. Updated `dotnet test` command to include JUnit XML logger. Added `JunitXml.TestLogger` package to multiple test projects. Integrated `Microsoft.Extensions.Http` and `MudBlazor` packages in `Askstatus.Web.App.csproj`. Refactored `MainLayout.razor` and `NavMenu.razor` to use MudBlazor components. Added `MainLayout.razor.cs` for layout logic. Updated `Weather.razor` to use `IHttpClientFactory`. Configured MudBlazor services and `HttpClient` in `Program.cs`. Updated `_Imports.razor`, `app.css`, `index.html`, and `manifest.webmanifest` for MudBlazor integration. Added package versions in `Directory.Packages.props`. Corrected project type GUIDs in `askstatus.sln`. Updated version properties in `version.props`.
    
    * Update test result location and add test report step
    
    Modified `dotnet test` to save results in `../../TestResults/`.
    Added "Test Report" step using `dorny/test-reporter@v1` to generate
    a test report from the `TestResults/` directory with `.xml` files.
    
    * Add authentication and update navigation
    
    Updated `Askstatus.Web.App.csproj` to include `Microsoft.AspNetCore.Components.WebAssembly.Authentication` package reference. Commented out the `Settings` navigation group in `NavMenu.razor`. Added `RedirectToLogin.razor` component to handle user redirection to the login page. Updated `_Imports.razor` to include necessary namespaces. Updated `Directory.Packages.props` with the new package version.

--- Askstatus.Web.App/Program.cs
+++ Askstatus.Web.App/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace Askstatus.Web.App;
 public class Program
@@ -9,8 +10,16 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddMudServices();
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Services.AddHttpClient("AskStatus.Web.API", client => client.BaseAddress = new Uri("https://localhost:7298"));
+        //.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+        //// Supply HttpClient instances that include access tokens when making requests to the server project
+        //builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Askstatus.Web.API"));
+
 
         await builder.Build().RunAsync();
     }
